b5581e4fdc508f3d4686012021bbb1b7bd4017be
Isabela Rozani Floriano <irf161295@gmail.com>
2022-07-26 20:38:27 -0300

    fix: ambiguous error in count songs, artists and albums

--- desafio2.sql
+++ desafio2.sql
@@ -1,6 +1,6 @@
-SELECT COUNT( DISTINCT song_id) AS cancoes,
-COUNT( DISTINCT artist_id) AS artistas,
-COUNT( DISTINCT album_id) AS albuns
+SELECT COUNT( DISTINCT SpotifyClone.Songs.song_id) AS cancoes,
+COUNT( DISTINCT SpotifyClone.Artists.artist_id) AS artistas,
+COUNT( DISTINCT SpotifyClone.Albums.album_id) AS albuns
 FROM SpotifyClone.Songs,
 SpotifyClone.Artists,
 SpotifyClone.Albums;
